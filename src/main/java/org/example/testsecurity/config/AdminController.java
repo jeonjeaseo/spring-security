3727913b2da2be5b8c7f5f7fc9bcd654a3b36c78
전제서 <wpti0416@gmail.com>
2025-09-10 14:57:27 +0900

    3

---
+++ src/main/java/org/example/testsecurity/config/AdminController.java
@@ -0,0 +1,16 @@
+package org.example.testsecurity.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String aminP() {
+
+        return "admin";
+    }
+
+
+}
---
+++ src/main/java/org/example/testsecurity/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.example.testsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티한테도 관리됨
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login").permitAll() // 모두 접근 가능
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
